d203cf2525a7a56b09508dedb81e5704e7d9745f
s9va87 <s9va87@mail.ru>
2020-11-01 23:13:21 -0500

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import Todolist from './Todolist'
 
-function App() {
+const App = () => {
+
+  const [todolist, setTodolist] = useState([
+    {id: 1, title: 'todo item1', status: false},
+    {id: 2, title: 'todo item2', status: true},
+    {id: 3, title: 'todo item3', status: false},
+  ])
+  const [newInput, setNewInput] = useState('');
+
+  const addNewTodoHandler = () => {
+    const newTodo = {id: Math.random(), title: newInput, status: false}
+    const newList = [...todolist, newTodo]
+    setTodolist(newList)
+    setNewInput('')
+  }
+  const inputHandler = (e) => {
+    setNewInput(e.target.value)
+  }
+
+  const deleteHandler = (todoId) => {
+    const newList = todolist.filter(el => todoId !== el.id)
+    setTodolist(newList)
+  }
+
+  const updateHandler = (todoId)=> {
+   const newList = todolist.map(el=> {
+     if (todoId === el.id) {
+         return {...el, status: !el.status}
+     }
+     return el
+       }
+   )
+      setTodolist(newList)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+      <div>
+        <input type='text' onChange={inputHandler} value={newInput}/>
+        <button onClick={addNewTodoHandler}>add new todo</button>
+        <Todolist
+            todolist={todolist}
+            deleteHandler={deleteHandler}
+            updateHandler={updateHandler}
+        />
+      </div>
+  )
 }
 
-export default App;
+
+export default App
